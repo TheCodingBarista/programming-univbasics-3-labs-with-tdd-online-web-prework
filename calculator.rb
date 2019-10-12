9f98f7e1f4e98aa78c98929288e1f9ace3a1205d
Melissa Holt <melissa.holt21@gmail.com>
2019-10-12 07:08:00 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
+first_number = 14
+second_number = 7
 
+def sum = first_number + second_number
+  puts "The sum is #{first_number + second_number}"
+end
+def difference = first_number - second_number
+  puts "The difference is #{first_number - second_number}."
+end
+def product = first_number * second_number
+  puts "The product is #{first_number * second_number}."
+end
+def quotient first_number / second_number
+  puts "The quotient is #{first_number / second_number}."
+end
