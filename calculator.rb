387f116c3d9589f83aae5ba5afaa6e300082b28d
Melissa Holt <melissa.holt21@gmail.com>
2019-10-12 07:11:14 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,15 +1,15 @@
 first_number = 14
 second_number = 7
 
-def sum(first_number, second_number)
-  puts "The sum is #{first_number + second_number}"
+def sum = (first_number + second_number)
+  puts "The sum is #{sum}"
 end
-def difference(first_number, second_number)
-  puts "The difference is #{first_number - second_number}."
+def difference = (first_number - second_number)
+  puts "The difference is #{difference}."
 end
-def product(first_number, second_number)
-  puts "The product is #{first_number * second_number}."
+def product = (first_number * second_number)
+  puts "The product is #{product}."
 end
-def quotient(first_number, second_number)
-  puts "The quotient is #{first_number / second_number}."
+def quotient= (first_number / second_number)
+  puts "The quotient is #{quotient}."
 end
